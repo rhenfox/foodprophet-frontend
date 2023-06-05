6db6f7f0a5e337319afd5c649bea4b591920afa8
rhenfox <aldrincabusog@gmail.com>
2023-06-05 09:40:21 +0800

    Co-authored-by: rhenfox <83150472+rhenfox@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import NavBar from './layout/NavBar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/addUser' element={<AddUser/>}/>
+          <Route exact path='/edituser/:id' element={<EditUser/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        loadUsers();
+    }, [])
+
+    const loadUsers = async () => {
+        const result = await axios.get('http://localhost:8080/users');
+        setUsers(result.data);
+    }
+    return (
+            <div className='container'>
+                <div className='px-2 py-2 shadow'>
+                    <div className='card px-2 py-2'>
+                        <p className='my-2 mx-2 float-right'>
+                            <Link className="btn btn-success pull-left" to='/addUser'><i className="fa fa-plus "></i>Add user</Link>
+                        </p>
+                        <table className='table table-hover border' >
+                            <thead >
+                                <tr className='table-primary'>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">User</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users.map((user, index) => (
+                                        <tr>
+                                            <th scope="row" key={index}>
+                                                {index + 1}
+                                            </th>
+                                            <td>{user.name}</td>
+                                            <td>{user.username}</td>
+                                            <td>{user.email}</td>
+                                            <th>
+                                                <button className='btn  mx-2 fa fa-search text-secondary'></button>
+
+                                                <Link className='btn fa fa-edit text-warning' to={`/edituser/${user.id}`}></Link>
+
+                                                <button className='btn  mx-2 fa fa-trash text-danger'></button>
+                                            </th>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+    )
+}
